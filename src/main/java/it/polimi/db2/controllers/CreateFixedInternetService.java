b4312704e524af8737d9c4af2a0a5fb2858a36b4
Arianna Dragoni <10611044@polimi.it>
2022-04-29 18:43:28 +0200

    handled exceptions in create fixed internet service for wrong inputs

--- src/main/java/it/polimi/db2/controllers/CreateFixedInternetService.java
+++ src/main/java/it/polimi/db2/controllers/CreateFixedInternetService.java
@@ -1,10 +1,19 @@
 package it.polimi.db2.controllers;
 
+import it.polimi.db2.entities.UserCustomer;
+import it.polimi.db2.entities.UserEmployee;
+import it.polimi.db2.exceptions.WrongIntException;
 import it.polimi.db2.services.ServicesService;
+import it.polimi.db2.services.UserEmployeeService;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 
 import javax.ejb.EJB;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,27 +29,66 @@ import static java.lang.Integer.parseInt;
 public class CreateFixedInternetService extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private static final int fixed = 1;
+    private TemplateEngine templateEngine;
 
     @EJB(name = "it.polimi.db2.services/InternetServiceService")
     private ServicesService sService;
-
+    private UserEmployeeService userEmployeeService;
 
     public CreateFixedInternetService(){
         super();
     }
 
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String name = StringEscapeUtils.escapeJava(request.getParameter("name"));
-        int gigaNum = parseInt(StringEscapeUtils.escapeJava(request.getParameter("gigaNum")));
-        float extraGigaFees = parseFloat(StringEscapeUtils.escapeJava(request.getParameter("extraFees")));
-        sService.addNewInternetService(name, gigaNum, extraGigaFees, fixed);
-
-        String ctxpath = request.getServletContext().getContextPath();
-        String path = ctxpath + "/home-page-employee";
-        response.sendRedirect(path);
+    public void init() {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
     }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        ServletContext servletContext = getServletContext();
+        final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+
+        // take the username
+        UserEmployee employee = (UserEmployee) request.getSession(false).getAttribute("employee");
+        request.getSession(false).setAttribute("employee", employee);
+        ctx.setVariable("loggedEmp", employee);
+
+
+        String serviceName = StringEscapeUtils.escapeJava(request.getParameter("name"));
+        String gigaNumString = StringEscapeUtils.escapeJava(request.getParameter("gigaNum"));
+        String extraGigaFeesString = StringEscapeUtils.escapeJava(request.getParameter("extraFees"));
+
+        String path;
+        if (gigaNumString.matches("[0-9]+") && extraGigaFeesString.matches("[+-]?([0-9]*[.])?[0-9]+")) {
+            int gigaNum = parseInt(gigaNumString);
+            float extraGigaFees = parseFloat(extraGigaFeesString);
+            sService.addNewInternetService(serviceName, gigaNum, extraGigaFees, fixed);
+            ctx.setVariable("wrongInt", "Service Correctly inserted!");
+            path = "/WEB-INF/HomePageEmployee.html";
+            templateEngine.process(path, ctx, response.getWriter());
+        } else if (!gigaNumString.matches("[0-9]+")){
+            ctx.setVariable("wrongInt", "Insert a number for GB!");
+            path = "/WEB-INF/HomePageEmployee.html";
+            templateEngine.process(path, ctx, response.getWriter());
+        } else if(!extraGigaFeesString.matches("[+-]?([0-9]*[.])?[0-9]+")){
+            ctx.setVariable("wrongInt", "Insert a number for GB fees!");
+            path = "/WEB-INF/HomePageEmployee.html";
+            templateEngine.process(path, ctx, response.getWriter());
+        } else if (!gigaNumString.matches("[0-9]+") && !extraGigaFeesString.matches("[+-]?([0-9]*[.])?[0-9]+")){
+            ctx.setVariable("wrongInt", "Insert a number for GB and GB fees!");
+            path = "/WEB-INF/HomePageEmployee.html";
+            templateEngine.process(path, ctx, response.getWriter());
+        }
+    }
+
+    // TODO gestire eccezioni id =
+    //problema ora nel nome del looged emp
 
 }
 
---
+++ src/main/java/it/polimi/db2/exceptions/WrongIntException.java
@@ -0,0 +1,10 @@
+package it.polimi.db2.exceptions;
+
+public class WrongIntException extends Exception{
+    private static final long serialVersionUID = 1L;
+
+    public WrongIntException(String message) {
+        super(message);
+    }
+
+}
