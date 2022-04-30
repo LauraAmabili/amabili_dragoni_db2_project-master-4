153521f1f5035d940141677f48cfeb229107decd
Arianna Dragoni <10611044@polimi.it>
2022-04-30 17:06:22 +0200

    show services and optional products for create new package service employee

---
+++ src/main/java/it/polimi/db2/controllers/CreateNewServicePackage.java
@@ -0,0 +1,39 @@
+package it.polimi.db2.controllers;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/create-service-package")
+public class CreateNewServicePackage extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    private TemplateEngine templateEngine;
+
+    public CreateNewServicePackage() {
+        super();
+    }
+
+    public void init() {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletContext servletContext = getServletContext();
+    }
+
+}
