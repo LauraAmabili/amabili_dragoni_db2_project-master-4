458d9702bcd1968ddbb71f4f51cb86c1612a6c73
LauraAmabili <laura.amabili@mail.polimi.it>
2022-05-04 00:27:48 +0200

    optional ordered

---
+++ src/main/java/it/polimi/db2/services/OptionalOrderedService.java
@@ -0,0 +1,30 @@
+package it.polimi.db2.services;
+
+import it.polimi.db2.entities.OptionalOrdered;
+import it.polimi.db2.entities.OptionalProduct;
+import it.polimi.db2.entities.ServicePackageOptional;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class OptionalOrderedService {
+
+    @PersistenceContext( unitName = "databaseEJB" )
+    private EntityManager em;
+
+    public OptionalOrderedService() { }
+
+
+    public void addOptionalProductToOrder(String optionalProduct, int order) {
+        OptionalOrdered optionalOrdered = new OptionalOrdered();
+        optionalOrdered.setOptionalProduct(optionalProduct);
+        optionalOrdered.setOrder(order);
+        em.persist(optionalOrdered);
+    }
+
+
+
+}
