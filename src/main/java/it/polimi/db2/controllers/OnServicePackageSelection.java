f8f3715a1aaa35bdad883e1eae3e5ec66d719247
LauraAmabili <laura.amabili@mail.polimi.it>
2022-05-04 10:07:09 +0200

    small changes

--- src/main/java/it/polimi/db2/controllers/OnServicePackageSelection.java
+++ src/main/java/it/polimi/db2/controllers/OnServicePackageSelection.java
@@ -125,6 +125,7 @@ public class OnServicePackageSelection extends HttpServlet {
             ctx.setVariable("mobilePhoneServices", mobilePhoneServices);
 
             templateEngine.process("/WEB-INF/AdditionalInformation.html", ctx, resp.getWriter());
+            //templateEngine.process("/WEB-INF/BuyService.html", ctx, resp.getWriter());
         }
 
     }
