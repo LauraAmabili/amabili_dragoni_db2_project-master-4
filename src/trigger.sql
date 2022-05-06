92d5c3eddd03535954406426ecc097a55db490a0
Arianna Dragoni <10611044@polimi.it>
2022-05-06 21:53:26 +0200

    trigger

--- src/trigger.sql
+++ src/trigger.sql
@@ -5,14 +5,18 @@ drop trigger if exists updateTotalPurchasesPerPacketValidityPeriod12;
 drop trigger if exists updateTotalPurchasesPerPacketValidityPeriod24;
 drop trigger if exists updateTotalPurchasesPerPacketValidityPeriod36;
 drop trigger if exists updateTotalPurchasesPerPacketTotalPackageSales;
-drop trigger if exists insertServicePackage;
 drop trigger if exists updateTotalPackageSales;
 drop trigger if exists updateTotalPackageSalesWithOp;
-drop trigger if exists updateTotalPackageSalesWithoutOp12;
+drop trigger if exists updateTotalPackageSalesWithoutOp;
+drop trigger if exists updateBestOptional;
+drop trigger if exists insertOptionalProduct;
+drop trigger if exists insertServicePackage;
+
 
 drop table if exists TotalPurchasesPerPacket;
 drop table if exists TotalPurchasesPerPacketValidityPeriod;
 drop table if exists TotalPackageSales;
+drop table if exists BestOptional;
 
 
 
@@ -82,7 +86,7 @@ create trigger updateTotalPackageSalesWithOp
         set totSalesWithOp = totSalesWithOp + (select o.TotalCost from Orders as o where new.orderId = o.orderId);
 
 delimiter $$
-create trigger updateTotalPackageSalesWithoutOp12
+create trigger updateTotalPackageSalesWithoutOp
     after insert on ActivationSchedule
     for each row
     begin
@@ -106,7 +110,28 @@ create trigger updateTotalPackageSalesWithoutOp12
 
 delimiter ;
 
+/*TODO List of insolvent users, suspended orders and alerts*/
 
+
+/*best seller optional products*/
+/*in the table are not ordered*/
+create table BestOptional(optionalProduct varchar(45) not null,
+                          pricePerMonth decimal(10,2) not null default 0.00,
+                          totSales decimal(10,2) not null default 0.00,
+                          foreign key (optionalProduct) references OptionalProduct(name) on delete cascade on update cascade,
+                          primary key (optionalProduct));
+
+delimiter $$
+create trigger updateBestOptional
+    after insert on ActivationSchedule
+    for each row
+    update BestOptional
+    set totSales = totSales + (select o.ValidityPeriodMonth from Orders as o where new.orderId = o.orderId ) * pricePerMonth
+    where optionalProduct in (select oo.optionalProductId from ServicePackage as sp, Orders as ord, OptionalOrdered as oo
+                                where new.orderId = ord.orderId and ord.orderId = oo.orderId);
+
+      /*and optionalProduct in (select oo.optionalProductId from OptionalOrdered as oowhere oo.orderId = ord.orderId));*/
+delimiter ;
 
 delimiter $$
 create trigger insertServicePackage
@@ -116,5 +141,15 @@ create trigger insertServicePackage
     insert into TotalPurchasesPerPacket(servicePackage,purchases) values(new.PackageName, default);
     insert into TotalPurchasesPerPacketValidityPeriod(servicePackage,purchases12, purchases24, purchases36) values(new.PackageName, default, default, default);
     insert into TotalPackageSales(servicePackage, totSalesWithOp, totSalesWithoutOP) values(new.PackageName, default, default);
+end $$
+delimiter ;
+
+
+delimiter $$
+create trigger insertOptionalProduct
+    after insert on OptionalProduct
+    for each row
+begin
+    insert into BestOptional(optionalProduct, pricePerMonth, totSales) values(new.name, new.montlyFee, default);
 end $$
 delimiter ;
